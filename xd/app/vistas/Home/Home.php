82a8615846592bbd0236bf84c0533cd96a745377
ChisusHD <103275087+ChisusHD@users.noreply.github.com>
2022-04-08 23:48:51 -0500

    portafolio
    Lore chulo

---
+++ xd/app/vistas/Home/Home.php
@@ -0,0 +1,27 @@
+<!-- Bloques de texto
+            Parrafos
+            Saltos de linea
+            Divisiones
+            Texto con formato
+            Alineacion de textos
+         -->
+         <!-- Parrafos -->
+         <p>
+       <!-- aqui puedo escribir  -->
+        </p>
+        <p>
+           <b>
+
+           Avance del Portafolio</b>
+           <h1>Hola</h1>
+           <h2>Soy goku</h2>
+           <h3>y necesito tu ayuda</h3>
+           <h4>Gracias</h4>
+           <h5>De nada </h5>
+           <h6></h6>
+           <h7></h7>
+        </p>
+
+        <div>
+
+        </div>
---
+++ xd/app/vistas/main/foot.php
@@ -0,0 +1,6 @@
+<script type="text/javascript" src="sys/libs/js/bootstrap.js"></script>
+<div class="titulo1"> 
+    <footer><!--escribir para el pie de pagina --></footer>
+</div>
+</body>  
+</html>
